930684be11ddea7a87163bc428ab4a472917fd25
Akash Sahu <akkusahu559@gmail.com>
2022-01-29 21:37:39 +0530

    UserLoginRequestModel Created

---
+++ PracticeCompleteJavaApp/src/main/java/com/akash/app/request/UserLoginRequestModel.java
@@ -0,0 +1,23 @@
+package com.akash.app.request;
+
+public class UserLoginRequestModel {
+	private String email;
+	private String Password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+}
